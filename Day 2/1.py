87fe2163ebd311ea50a7166d51625829a33c7196
Rohan <rohannyati65@gmail.com>
2022-01-27 18:21:44 +0530

    changes

--- Day 2/1.py
+++ Day 2/1.py
@@ -1,1 +1,7 @@
-# (Leetcode)
+# 1480. Running Sum of 1d Array(Leetcode)
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        l = []
+        for i in range(len(nums)):
+            l.append(sum(nums[: i + 1]))
+        return l
