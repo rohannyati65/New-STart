0d7133b23d8f5da29a6f7255f157406d7af3421e
Rohan <rohannyati65@gmail.com>
2022-01-27 19:44:00 +0530

    changes

--- Day 2/2.py
+++ Day 2/2.py
@@ -1,4 +1,13 @@
 # 724. Find Pivot Index (leetcode)
+# Here in this question if we use the sum function again and again we
+# will get a TLE for our last test case.
+# So to avoid that its better to take a random variable and assign each
+# index value to this variable to get the left side sum and for right side
+# for once take the sum of entire array and store its value in this
+# variable and for every index value reduce that index value from this
+# variable to get the right side sum for that particular index.
+
+
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         a = sum(nums)
