c00f35236cf7dcde0b3f88ec404dd9b5ab9661a0
Rohan <rohannyati65@gmail.com>
2022-01-28 10:23:20 +0530

    changes

--- Day 3/1.py
+++ Day 3/1.py
@@ -1,4 +1,16 @@
 # 122. Best Time to Buy and Sell Stock II (leetcode)
+
+# Here in this solution we try to find different pivot points and as per
+# them we keep on changing the value for our y variable (i.e. the point
+# in time where we should buy our stocks at lowest price).
+
+# Till we find a pivot point (condition where the previous value was
+# bigger then the current value) we keep on finding the max value for our
+# price till that time and store it in a temporary variable "a" for the
+# time being , and then keep on adding this value of "a" to "p" as soon
+# as we hit a pivot point to get the max value of prices .
+
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         p = 0
