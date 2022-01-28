8963c27e3b0c5553f7653b2df6b0ea834ea405c0
Rohan <rohannyati65@gmail.com>
2022-01-28 07:46:43 +0530

    changes

--- Day 3/2.py
+++ Day 3/2.py
@@ -1,1 +1,7 @@
-# (leetcode)
+# 977. Squares of a Sorted Array (leetcode)
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            nums[i] = nums[i] * nums[i]
+        nums.sort()
+        return nums
