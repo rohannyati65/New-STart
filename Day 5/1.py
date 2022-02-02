1f0ef34792d692e27622971dbd91fba96b2c77bc
Rohan <rohannyati65@gmail.com>
2022-02-02 23:49:41 +0530

    changes

--- Day 5/1.py
+++ Day 5/1.py
@@ -35,4 +35,32 @@ class Solution:
         return l
 
 
-# 3rd Attempt
+# 3rd Attempt (Accepted)
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        if len(s) < len(p):
+            return []
+        pCount = {}
+        sCount = {}
+        for i in range(len(p)):
+            pCount[p[i]] = 1 + pCount.get(p[i], 0)
+            sCount[s[i]] = 1 + sCount.get(s[i], 0)
+
+        if pCount == sCount:
+            res = [0]
+        else:
+            res = []
+
+        l = 0
+        for r in range(len(p), len(s)):
+            sCount[s[r]] = 1 + sCount.get(s[r], 0)
+            sCount[s[l]] -= 1
+
+            if sCount[s[l]] == 0:
+                sCount.pop(s[l])
+
+            l = l + 1
+            if sCount == pCount:
+                res.append(l)
+
+        return res
