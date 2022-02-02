b8af3c0ddb4f5aa9bbfc16ab30203af7a7e5e5e6
Rohan <rohannyati65@gmail.com>
2022-02-02 21:21:14 +0530

    changes

--- Day 4/1.py
+++ Day 4/1.py
@@ -1,1 +1,7 @@
-#  (leetcode)
+# 1672. Richest Customer Wealth (leetcode)
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        l = [0]
+        for i in accounts:
+            l.append(sum(i))
+        return max(l)
