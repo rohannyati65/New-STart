a032085761845a09c89859471d4530d9f54e3198
Rohan <rohannyati65@gmail.com>
2022-02-07 13:12:31 +0530

    changes

---
+++ Day 9/2.py
@@ -0,0 +1,25 @@
+# 389. Find the Difference (leetcode)
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        if(len(s)==0):
+            return(t)
+        
+        s=[i for i in s]
+        t=[i for i in t]
+        
+        l1=list(set(s))
+        l2=list(set(t))
+        
+        l1.sort()
+        l2.sort()
+        x=""
+        
+        for i in l2:
+            a=t.count(i)
+            b=s.count(i)
+            if(a!=b):
+                c=a-b
+                #print(i)
+                x=x+i*c
+                break
+        return(x)
